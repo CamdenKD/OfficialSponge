4c1b13fbd8df054306144ff028f7aa1bef2820c5
CamdenKD <camdenkerndavis@gmail.com>
2014-09-09 16:43:08 -0400

    This is the official Sponge API

---
+++ Java Code/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld 
+
+{
+	static String randomString = "String to print";
+	
+	static final double CRAZYNUM = 24242.2442242;
+	
+	public static void main(String[] args)
+	{
+		System.out.println(CRAZYNUM);
+		}
+}
